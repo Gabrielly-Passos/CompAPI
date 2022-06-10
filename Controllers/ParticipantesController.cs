4c03775a909b7d2332535e387dfe3fa619e53c24
Gabrielly-passos <passosgabrielly03@gmail.com>
2022-06-09 21:24:29 -0300

    [Create] Partcipantes

---
+++ Controllers/ParticipantesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompAPI.Controllers
+{
+         [ApiConTtroller]
+        [Route("[controller]")]
+
+    public class ParticipantesController : controllerBase
+    {
+       private reandoly IConfiguration _config;
+
+
+    }
+}
